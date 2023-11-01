d89616816eebd7b9333874c4015bb3932d08b423
parimal123 <parimal.d.deshmukh>
2023-11-01 10:57:46 +0530

    ajio shopping application

---
+++ src/main/java/com/reliance/ajio/AjioApplication.java
@@ -0,0 +1,13 @@
+package com.reliance.ajio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reliance/ajio/AjioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reliance.ajio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
